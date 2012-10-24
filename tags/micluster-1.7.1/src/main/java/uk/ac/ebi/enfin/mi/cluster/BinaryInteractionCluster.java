6fe7fbc613efa93de3eb392b48882d93c4869d91
noedelta@gmail.com <noedelta@gmail.com@3c1daf90-96cb-a421-83be-6128f888c089>
2012-10-24 23:34:07 +0000

    [maven-release-plugin]  copy for tag micluster-1.7.1

---
+++ tags/micluster-1.7.1/src/main/java/uk/ac/ebi/enfin/mi/cluster/BinaryInteractionCluster.java
@@ -0,0 +1,54 @@
+package uk.ac.ebi.enfin.mi.cluster;
+
+import psidev.psi.mi.tab.model.BinaryInteraction;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.Reader;
+import java.io.Serializable;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Interface for interaction cluster
+ *
+ * @author Marine Dumousseau (marine@ebi.ac.uk)
+ * @version $Id$
+ * @since <pre>01/04/11</pre>
+ */
+
+public interface BinaryInteractionCluster<T extends EncoreBinaryInteraction> extends Serializable{
+
+    public void runService();
+
+    public Map<Integer, T> getInteractionMapping();
+
+    public void setInteractionMapping(Map<Integer, T> interactionMapping);
+
+    public Map<String, List<Integer>> getInteractorMapping();
+
+    public void setInteractorMapping(Map<String, List<Integer>> interactorMapping);
+
+    public int getInteractionMappingId();
+
+    public void setInteractionMappingId(int interactionMappingId);
+
+    public Map<String, String> getSynonymMapping();
+    public void setSynonymMapping(Map<String, String> synonymMapping);
+
+    public void setMappingIdDbNames( String mappingIdDbNames );
+
+    public String getMappingIdDbNames();
+
+    public Iterator<BinaryInteraction> getBinaryInteractionIterator();
+
+    public void setBinaryInteractionIterator(Iterator<BinaryInteraction> binaryInteractionIterator);
+
+    public void setBinaryInteractionIterator(InputStream is, boolean hasHeader) throws ClusterServiceException;
+
+    public void setBinaryInteractionIterator(Reader r, boolean hasHeader) throws ClusterServiceException;
+
+    public void saveResultsInMitab(String fileName) throws IOException;
+
+}
