bc720effdc194fac4ccfb756eb174f924ad7c156
oscar.forner.martinez@gmail.com <oscar.forner.martinez@gmail.com@3c1daf90-96cb-a421-83be-6128f888c089>
2014-06-13 10:41:59 +0000

    Removed previous stuff

--- trunk/micluster-jami/src/main/java/uk/ac/ebi/enfin/mi/cluster/model/ClusterComparator.java
+++
@@ -1,9 +0,0 @@
-package uk.ac.ebi.enfin.mi.cluster.model;
-
-import java.util.Comparator;
-
-/**
- * Created by maitesin on 08/06/14.
- */
-public interface ClusterComparator extends Comparator<EncoreInteraction> {
-}
