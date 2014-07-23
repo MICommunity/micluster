473e86491c24b857ea07ceacd89115b3cf09d7e9
Oscar Forner Martinez <oscar.forner.martinez@gmail.com>
2014-07-23 16:07:50 +0000

    new stuff and deleted to avoid input/output (use only JAMI objects)

--- micluster-jami/src/main/java/psidev/psi/mi/jami/cluster/exception/ClusterException.java
+++ micluster-jami/src/main/java/psidev/psi/mi/jami/cluster/exception/ClusterException.java
@@ -4,8 +4,7 @@ package psidev.psi.mi.jami.cluster.exception;
  * Created by maitesin on 12/06/2014.
  */
 public class ClusterException extends Exception {
-    public ClusterException() {
-    }
+    public ClusterException() {}
 
     public ClusterException(String message) {
         super(message);
--- micluster-jami/src/main/java/psidev/psi/mi/jami/cluster/score/DefaultMIScoreStrategy.java
+++ micluster-jami/src/main/java/psidev/psi/mi/jami/cluster/score/DefaultMIScoreStrategy.java
@@ -1,6 +1,6 @@
 package psidev.psi.mi.jami.cluster.score;
 
-import psidev.psi.mi.jami.model.Interactor;
+import psidev.psi.mi.jami.model.Interaction;
 
 /**
  * Created by maitesin on 18/07/2014.
@@ -12,7 +12,7 @@ public class DefaultMIScoreStrategy extends AbstractMIScoreStrategy {
     public DefaultMIScoreStrategy(String filename) { super(filename); }
 
     @Override
-    public Double getInteractorScore(Interactor interactor) {
+    public Double getInteractorScore(Interaction interaction) {
         Double result = 0.0d;
         String type = null;//TODO: get type from the interator
         String method = null;//TODO: get method from the interactor
