1447709548ef63c937d6e53eeb4fe18ef8c744a7
Oscar Forner Martinez <oscar.forner.martinez@gmail.com>
2014-07-23 13:29:06 +0000

    Changes and test in the score module (not finished)

--- micluster-jami/src/main/java/psidev/psi/mi/jami/cluster/io/output/DefaultMitabClusterOutput.java
+++ micluster-jami/src/main/java/psidev/psi/mi/jami/cluster/io/output/DefaultMitabClusterOutput.java
@@ -10,49 +10,49 @@ import java.util.Map;
 /**
  * Created by maitesin on 17/07/2014.
  */
-public class DefaultMitabClusterOutput extends AbstractMitabClusterOutput implements ClusterOutput {
-    @Override
-    public void initialiseContext(Map map) {
-        super.initialiseContext(map);
-    }
-
-    @Override
-    public void start() throws MIIOException {
-        super.start();
-    }
-
-    @Override
-    public void end() throws MIIOException {
-        super.end();
-    }
-
-    @Override
-    public void write(Interaction interaction) throws MIIOException {
-
-    }
-
-    @Override
-    public void write(Collection collection) throws MIIOException {
-
-    }
-
-    @Override
-    public void write(Iterator iterator) throws MIIOException {
-
-    }
-
-    @Override
-    public void flush() throws MIIOException {
-        super.flush();
-    }
-
-    @Override
-    public void close() throws MIIOException {
-        super.close();
-    }
-
-    @Override
-    public void reset() throws MIIOException {
-        super.reset();
-    }
-}
+//public class DefaultMitabClusterOutput extends AbstractMitabClusterOutput implements ClusterOutput {
+//    @Override
+//    public void initialiseContext(Map map) {
+//        super.initialiseContext(map);
+//    }
+//
+//    @Override
+//    public void start() throws MIIOException {
+//        super.start();
+//    }
+//
+//    @Override
+//    public void end() throws MIIOException {
+//        super.end();
+//    }
+//
+//    @Override
+//    public void write(Interaction interaction) throws MIIOException {
+//
+//    }
+//
+//    @Override
+//    public void write(Collection collection) throws MIIOException {
+//
+//    }
+//
+//    @Override
+//    public void write(Iterator iterator) throws MIIOException {
+//
+//    }
+//
+//    @Override
+//    public void flush() throws MIIOException {
+//        super.flush();
+//    }
+//
+//    @Override
+//    public void close() throws MIIOException {
+//        super.close();
+//    }
+//
+//    @Override
+//    public void reset() throws MIIOException {
+//        super.reset();
+//    }
+//}
--- micluster-jami/src/main/java/psidev/psi/mi/jami/cluster/score/AbstractMIScore.java
+++ micluster-jami/src/main/java/psidev/psi/mi/jami/cluster/score/AbstractMIScore.java
@@ -1,16 +1,76 @@
 package psidev.psi.mi.jami.cluster.score;
 
+import java.io.IOException;
+import java.util.HashMap;
 import java.util.Map;
+import java.util.Properties;
 
 /**
  * Created by maitesin on 18/07/2014.
  */
 public abstract class AbstractMIScore implements MIScore {
+    /***********************/
+    /***   Constructor   ***/
+    /***********************/
     public AbstractMIScore(String filename){
-        //TODO: Load values of the file into the Maps
+        //Set the default values
+        this.types = new HashMap<String, MIScoreProperty>();
+        this.methods = new HashMap<String, MIScoreProperty>();
+        this.typeWeight = 1.0d;
+        this.methodWeight = 1.0d;
+        //Process the file
+        Properties properties = new Properties();
+        String [] fields = null; //id,name,score,unNormalizedScore
+        try {
+            properties.load(this.getClass().getClassLoader().getResourceAsStream(filename));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        fields = properties.getProperty("properties").split(",");
+        for (Object key : properties.keySet()) {
+            String skey = key.toString();
+            if(skey.startsWith("type") && skey.endsWith(fields[0])){
+                //Type property
+                insertTypeProperty(properties, key, fields);
+            }
+            else if(skey.startsWith("method") && skey.endsWith(fields[0])){
+                //Method property
+                insertMethodProperty(properties, key, fields);
+            }
+        }
     }
-    protected Map<String, Double> methods;
-    protected Map<String, Double> types;
+
+    /***************************/
+    /***   Private Methods   ***/
+    /***************************/
+    private void insertTypeProperty(Properties properties, Object key, String[] fields) {
+        String prefix = key.toString().substring(0, key.toString().length()-fields[0].length());
+        this.types.put(properties.getProperty(key.toString()), getMIScoreproperty(properties, prefix, fields));
+    }
+
+    private void insertMethodProperty(Properties properties, Object key, String[] fields) {
+        String prefix = key.toString().substring(0, key.toString().length()-fields[0].length());
+        this.methods.put(properties.getProperty(key.toString()), getMIScoreproperty(properties, prefix, fields));
+    }
+
+
+    private MIScoreProperty getMIScoreproperty(Properties properties, String prefix, String[] fields) {
+        MIScoreProperty property = new MIScoreProperty();
+        if (fields.length > 4) {
+            property.setId(properties.getProperty(prefix + fields[0])); //ID
+            property.setName(properties.getProperty(prefix + fields[1])); //Name
+            property.setScore(Double.parseDouble(properties.getProperty(prefix + fields[2]))); //Score
+            property.setUnNormalizedScore(Double.parseDouble(properties.getProperty(prefix + fields[3]))); //Unnormalized Score
+        }
+        return property;
+    }
+
+
+    /********************************/
+    /***   Protected Attributes   ***/
+    /********************************/
+    protected Map<String, MIScoreProperty> methods;
+    protected Map<String, MIScoreProperty> types;
     protected Double methodWeight;
     protected Double typeWeight;
 
