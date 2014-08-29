6a64395f2b6064cf37f6f49c40a5b48e2e4e9383
oscar.forner.martinez@gmail.com <oscar.forner.martinez@gmail.com@3c1daf90-96cb-a421-83be-6128f888c089>
2014-08-29 12:48:39 +0000

    Added methods in InteractionClusterUtils to build InteractionClusterSummary from InteractionCluster objects

--- trunk/micluster-jami/src/main/java/psidev/psi/mi/jami/cluster/model/summary/DefaultParticipantClusterSummary.java
+++ trunk/micluster-jami/src/main/java/psidev/psi/mi/jami/cluster/model/summary/DefaultParticipantClusterSummary.java
@@ -2,7 +2,6 @@ package psidev.psi.mi.jami.cluster.model.summary;
 
 import psidev.psi.mi.jami.model.*;
 
-import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -16,48 +15,48 @@ public class DefaultParticipantClusterSummary extends AbstractParticipantCluster
     /***   Constructor   ***/
     /***********************/
     public DefaultParticipantClusterSummary() {
-        this.biologicalRole2ExperimentMap = new HashMap<CvTerm, List<Experiment>>();
-        this.experimentalRole2ExperimentMap = new HashMap<CvTerm, List<Experiment>>();
-        this.feature2ExperimentMap = new HashMap<Feature, Experiment>();
-        this.identificationMethod2ExperimentMap = new HashMap<CvTerm, List<Experiment>>();
-        this.interactor2BioRoleMap = new HashMap<Interactor, List<CvTerm>>();
-        this.interactor2ExperimentalRoleMap = new HashMap<Interactor, List<CvTerm>>();
-        this.organism2ExperimentMap = new HashMap<Organism, List<Experiment>>();
+        this.biologicalRole2Experiment = new HashMap<CvTerm, List<Experiment>>();
+        this.experimentalRole2Experiment = new HashMap<CvTerm, List<Experiment>>();
+        this.feature2Experiment = new HashMap<Feature, Experiment>();
+        this.identificationMethod2Experiment = new HashMap<CvTerm, List<Experiment>>();
+        this.interactor2BioRole = new HashMap<Interactor, List<CvTerm>>();
+        this.interactor2ExperimentalRole = new HashMap<Interactor, List<CvTerm>>();
+        this.organism2Experiment = new HashMap<Organism, List<Experiment>>();
     }
 
     /*******************/
     /***   Getters   ***/
     /*******************/
-    public Map<CvTerm, List<Experiment>> getBiologicalRole2ExperimentMap() {
-        return biologicalRole2ExperimentMap;
+    public Map<CvTerm, List<Experiment>> getBiologicalRole2Experiment() {
+        return biologicalRole2Experiment;
     }
-    public Map<CvTerm, List<Experiment>> getExperimentalRole2ExperimentMap() {
-        return experimentalRole2ExperimentMap;
+    public Map<CvTerm, List<Experiment>> getExperimentalRole2Experiment() {
+        return experimentalRole2Experiment;
     }
-    public Map<Feature, Experiment> getFeature2ExperimentMap() {
-        return feature2ExperimentMap;
+    public Map<Feature, Experiment> getFeature2Experiment() {
+        return feature2Experiment;
     }
-    public Map<CvTerm, List<Experiment>> getIdentificationMethod2ExperimentMap() {
-        return identificationMethod2ExperimentMap;
+    public Map<CvTerm, List<Experiment>> getIdentificationMethod2Experiment() {
+        return identificationMethod2Experiment;
     }
-    public Map<Interactor, List<CvTerm>> getInteractor2BioRoleMap() {
-        return interactor2BioRoleMap;
+    public Map<Interactor, List<CvTerm>> getInteractor2BioRole() {
+        return interactor2BioRole;
     }
-    public Map<Interactor, List<CvTerm>> getInteractor2ExperimentalRoleMap() {
-        return interactor2ExperimentalRoleMap;
+    public Map<Interactor, List<CvTerm>> getInteractor2ExperimentalRole() {
+        return interactor2ExperimentalRole;
     }
-    public Map<Organism, List<Experiment>> getOrganism2ExperimentMap() {
-        return organism2ExperimentMap;
+    public Map<Organism, List<Experiment>> getOrganism2Experiment() {
+        return organism2Experiment;
     }
 
     /******************************/
     /***   Private Attributes   ***/
     /******************************/
-    private Map<CvTerm, List<Experiment>> biologicalRole2ExperimentMap;
-    private Map<CvTerm, List<Experiment>> experimentalRole2ExperimentMap;
-    private Map<Feature, Experiment> feature2ExperimentMap;
-    private Map<CvTerm,List<Experiment>> identificationMethod2ExperimentMap;
-    private Map<Interactor, List<CvTerm>> interactor2BioRoleMap;
-    private Map<Interactor, List<CvTerm>> interactor2ExperimentalRoleMap;
-    private Map<Organism,List<Experiment>> organism2ExperimentMap;
+    private Map<CvTerm, List<Experiment>> biologicalRole2Experiment;
+    private Map<CvTerm, List<Experiment>> experimentalRole2Experiment;
+    private Map<Feature, Experiment> feature2Experiment;
+    private Map<CvTerm, List<Experiment>> identificationMethod2Experiment;
+    private Map<Interactor, List<CvTerm>> interactor2BioRole;
+    private Map<Interactor, List<CvTerm>> interactor2ExperimentalRole;
+    private Map<Organism,List<Experiment>> organism2Experiment;
 }
--- trunk/micluster-jami/src/main/java/psidev/psi/mi/jami/cluster/util/InteractionClusterUtils.java
+++ trunk/micluster-jami/src/main/java/psidev/psi/mi/jami/cluster/util/InteractionClusterUtils.java
@@ -5,19 +5,18 @@ import psidev.psi.mi.jami.cluster.model.summary.DefaultInteractionClusterSummary
 import psidev.psi.mi.jami.cluster.model.summary.DefaultParticipantClusterSummary;
 import psidev.psi.mi.jami.cluster.model.summary.InteractionClusterSummary;
 import psidev.psi.mi.jami.cluster.model.summary.ParticipantClusterSummary;
-import psidev.psi.mi.jami.model.Feature;
-import psidev.psi.mi.jami.model.Interaction;
-import psidev.psi.mi.jami.model.Participant;
+import psidev.psi.mi.jami.model.*;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Iterator;
+import java.util.*;
 
 /**
  * Created by maitesin on 17/07/2014.
  */
 public class InteractionClusterUtils {
 
+    /**************************/
+    /***   Public Methods   ***/
+    /**************************/
     public static Collection<InteractionClusterSummary> buildSummaries(Iterator<InteractionCluster<Interaction>> interactionClusters) {
         Collection<InteractionClusterSummary> summaries = new ArrayList<InteractionClusterSummary>();
         while (interactionClusters.hasNext()){
@@ -34,10 +33,15 @@ public class InteractionClusterUtils {
         while (interactions.hasNext()){
             //Get Interaction
             inter = interactions.next();
-            //Experiments
-            //TODO
-            //Interaction Detection Method 2 Publications(Experiments)
-            //TODO
+            if(inter instanceof InteractionEvidence) {
+                InteractionEvidence interactionEvidence = (InteractionEvidence) inter;
+                //Experiments
+                summary.getExperiments().add(interactionEvidence.getExperiment());
+                //Interaction Detection Method 2 Publications(Experiments)
+                addExperimen2Map(summary.getInteractionDetectionMethod2Publication(),interactionEvidence.getExperiment().getInteractionDetectionMethod(), interactionEvidence.getExperiment());
+                //Publication
+                summary.getPublications().add(interactionEvidence.getExperiment().getPublication());
+            }
             //Interaction Type
             summary.getInteractionTypes().add(inter.getInteractionType());
             //Participants
@@ -45,31 +49,78 @@ public class InteractionClusterUtils {
             while(participantIterator.hasNext()) {
                 summary.getParticipants().add(InteractionClusterUtils.buildParticipantSummary(participantIterator.next()));
             }
-            //Publication
-            //TODO
         }
         return summary;
     }
 
     public static ParticipantClusterSummary buildParticipantSummary(Participant participant){
         DefaultParticipantClusterSummary summary = new DefaultParticipantClusterSummary();
-        //Biological Role 2 Experiments
-        //summary.getBiologicalRole2ExperimentMap().put(participant.getBiologicalRole(), );
-        //Experimental Role 2 Experiments
-        //TODO
-        //Feature 2 Experiment
-        for(Feature feature : (Collection<Feature>) participant.getFeatures()){
-            //summary.getFeature2ExperimentMap().put(feature, );
+        Interaction inter = participant.getInteraction();
+        if(inter instanceof InteractionEvidence) {
+            InteractionEvidence interactionEvidence = (InteractionEvidence) inter;
+            //Biological Role 2 Experiments
+            addExperimen2Map(summary.getBiologicalRole2Experiment(), participant.getBiologicalRole(),interactionEvidence.getExperiment());
+            if(participant instanceof ParticipantEvidence) {
+                ParticipantEvidence participantEvidence = (ParticipantEvidence) participant;
+                //Experimental Role 2 Experiments
+                addExperimen2Map(summary.getExperimentalRole2Experiment(), participantEvidence.getExperimentalRole(), interactionEvidence.getExperiment());
+                //Identification Method 2 Experiments
+                for (CvTerm identificationMethod : participantEvidence.getIdentificationMethods()){
+                    addExperimen2Map(summary.getIdentificationMethod2Experiment(), identificationMethod, interactionEvidence.getExperiment());
+                }
+                //Interactor 2 Experimental Roles
+                addRole2Map(summary.getInteractor2ExperimentalRole(), participant.getInteractor(), participantEvidence.getExperimentalRole());
+            }
+            //Feature 2 Experiment
+            for (Feature feature : (Collection<Feature>) participant.getFeatures()) {
+                summary.getFeature2Experiment().put(feature, interactionEvidence.getExperiment());
+            }
+            //Interactor 2 Bio Roles
+            addRole2Map(summary.getInteractor2BioRole(),participant.getInteractor(), participant.getBiologicalRole());
+            //Organism 2 Experiments
+            if(summary.getOrganism2Experiment().containsKey(participant.getInteractor().getOrganism())){
+                //It is already in the map
+                summary.getOrganism2Experiment().get(participant.getInteractor().getOrganism()).add(interactionEvidence.getExperiment());
+            }
+            else{
+                //New entry in the map
+                List<Experiment> experiments = new ArrayList<Experiment>();
+                experiments.add(interactionEvidence.getExperiment());
+                summary.getOrganism2Experiment().put(participant.getInteractor().getOrganism(),experiments);
+            }
+
         }
-        //Identification Method 2 Experiments
-        //TODO
-        //Interactor 2 Bio Roles
-        //TODO
-        //Interactor 2 Experimental Roles
-        //TODO
-        //Organism 2 Experiments
-        //TODO
         return summary;
     }
+
+    /***************************/
+    /***   Private Methods   ***/
+    /***************************/
+    private static void addExperimen2Map(Map<CvTerm,List<Experiment>> map, CvTerm cvTerm, Experiment experiment){
+        if(map.containsKey(cvTerm)){
+            //It is already in the map
+            map.get(cvTerm).add(experiment);
+        }
+        else{
+            //New entry in the map
+            List<Experiment> experiments = new ArrayList<Experiment>();
+            experiments.add(experiment);
+            map.put(cvTerm, experiments);
+        }
+    }
+
+    private static void addRole2Map(Map<Interactor,List<CvTerm>> map, Interactor interactor, CvTerm role){
+        if(map.containsKey(interactor)){
+            //It is already in the map
+            map.get(interactor).add(role);
+        }
+        else {
+            //New entry in the map
+            List<CvTerm> roles = new ArrayList<CvTerm>();
+            roles.add(role);
+            map.put(interactor,roles);
+        }
+    }
+
 
 }
