5cd122da5dd2c0c37f501c62f75098f7d61ed82e
oscar.forner.martinez@gmail.com <oscar.forner.martinez@gmail.com@3c1daf90-96cb-a421-83be-6128f888c089>
2015-03-02 16:46:55 +0000

    Changes in the MIScore. Still work to do in the MIScore, but at least it can calculate some scores

--- trunk/micluster-jami/src/main/java/psidev/psi/mi/jami/cluster/score/DefaultMIScoreCalculator.java
+++ trunk/micluster-jami/src/main/java/psidev/psi/mi/jami/cluster/score/DefaultMIScoreCalculator.java
@@ -1,12 +1,18 @@
 package psidev.psi.mi.jami.cluster.score;
 
 import psidev.psi.mi.jami.cluster.model.DefaultInteractionCluster;
+import psidev.psi.mi.jami.cluster.model.InteractionCluster;
+import psidev.psi.mi.jami.model.Experiment;
 import psidev.psi.mi.jami.model.Interaction;
+import psidev.psi.mi.jami.model.InteractionEvidence;
+import psidev.psi.mi.jami.model.Participant;
+import psidev.psi.mi.jami.model.impl.DefaultExperiment;
+import psidev.psi.mi.jami.model.impl.DefaultPublication;
 
 /**
  * Created by maitesin on 04/08/2014.
  */
-public class DefaultMIScoreCalculator<I extends Interaction,T extends DefaultInteractionCluster<I>> extends AbstractMIScoreCalculator<T> {
+public class DefaultMIScoreCalculator<I extends Interaction,T extends InteractionCluster<I>> extends AbstractMIScoreCalculator<T> {
     /***********************/
     /***   Constructor   ***/
     /***********************/
@@ -20,9 +26,13 @@ public class DefaultMIScoreCalculator<I extends Interaction,T extends DefaultInt
         double score = 0.0d;
 
         for(I interaction : interactions.getInteractions()){
-            //TODO: extract methods and type from interaction
-            //this.miScore.addMethod();
-            //this.miScore.addType();
+            if (interaction instanceof InteractionEvidence) {
+                InteractionEvidence evidence = (InteractionEvidence) interaction;
+                this.getMiScore().addMethod(evidence.getExperiment().getInteractionDetectionMethod().getMIIdentifier());
+            }
+            if (interaction.getInteractionType() != null) {
+                this.getMiScore().addType(interaction.getInteractionType().getMIIdentifier());
+            }
         }
 
         score += this.getMiScore().getMethodsScore();
